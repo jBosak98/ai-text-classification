6b0f745915368c998340e6745b4cd6c1f474e19a
jBosak <jbosak98@gmail.com>
2020-06-12 21:13:14 +0200

    save-to-arff function

--- textsToArff.clj
+++ textsToArff.clj
@@ -16,10 +16,30 @@
     {} words))
 
 
+(defn get-category-occurences [category-name best-words resources-path]
+  (def category-words (count-words (load-articles (str resources-path category-name))))
+  (str (str/join "," (map (fn [word] (or (get category-words (first word)) 0)) best-words)) "," category-name))
+
+(defn categories-attr [categories] (str "{" (str/join "," categories) "}"))
+
+(defn arff-string [best-words categories resources-path]
+          (def file-header "@RELATION iris\n\n")
+          (def attrs (str/join "\n" (map #(str "@ATTRIBUTE " (first %1) " NUMERIC") best-words)))
+          (def cat-attr (categories-attr categories))
+          (def data (str/join "\n" (map #(get-category-occurences %1 best-words resources-path) categories)))
+          (str file-header attrs cat-attr "\n\n" data))
+
+(defn save-arff [content filename]
+  (with-open [w (clojure.java.io/writer  filename :append true)]
+    (.write w content)))
+
 (defn main []
   (def resources-path "./resources/")
   (def categories (get-categories resources-path))
   (def all-words (flatten (map #(load-articles  (str resources-path %1)) categories)))
-  (println all-words))
+  (def best-words (take 10000 (sort-by second #(compare %2 %1) (count-words all-words))))
+  (def arff-text (arff-string best-words categories resources-path))
+  (save-arff arff-text "data.arff")
+  )
 
 (main)
